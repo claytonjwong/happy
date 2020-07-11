09c4f789a041c75bb4666cc12d0f6b1a093693d5
Clayton Wong <claytonjwong@gmail.com>
2020-07-10 18:09:37 -0700

    rename 'Main' -> 'Home' and use Home component in the react navigation

--- App.js
+++ App.js
@@ -2,14 +2,7 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-const HomeScreen = () => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-};
+import Home from './components/Home';
 
 const Stack = createStackNavigator();
 
@@ -17,7 +10,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Home' component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Home Screen</Text>
+        </View>
+    );
+  }
+}
+
+export default Home;
