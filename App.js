1a97b8eaac6de8ab84ae77856ea5256c3734501f
Clayton Wong <claytonjwong@gmail.com>
2020-07-10 19:01:01 -0700

    experiment with changing the header options

--- App.js
+++ App.js
@@ -15,12 +15,83 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Annoying' component={Annoying} options={{ title: 'Mind\'s Annoying Features' }} />
-        <Stack.Screen name='Biases' component={Biases} options={{ title: 'Overcoming Biases' }} />
-        <Stack.Screen name='Habits' component={Habits} options={{ title: 'Better Habits' }} />
-        <Stack.Screen name='Home' component={Home} options={{ title: 'Home' }} />
-        <Stack.Screen name='Rewirements' component={Rewirements} options={{ title: 'Rewirements' }} />
-        <Stack.Screen name='Wanting' component={Wanting} options={{ title: 'Better Wanting' }} />
+        <Stack.Screen
+          name='Annoying'
+          component={Annoying}
+          options={{
+            title: 'Mind\'s Annoying Features',
+            headerStyle: {
+              backgroundColor: '#ff0000',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen
+          name='Biases'
+          component={Biases}
+          options={{
+            title: 'Overcoming Biases',
+            headerStyle: {
+              backgroundColor: '#ff7f00',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen
+          name='Habits'
+          component={Habits}
+          options={{
+            title: 'Better Habits',
+            headerStyle: {
+              backgroundColor: '#ffff00',
+            },
+            headerTintColor: '#000',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen
+          name='Home'
+          component={Home}
+          options={{
+            title: 'Home'
+          }}
+        />
+        <Stack.Screen
+          name='Rewirements'
+          component={Rewirements}
+          options={{
+            title: 'Rewirements',
+            headerStyle: {
+              backgroundColor: '#00ff00',
+            },
+            headerTintColor: '#000',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen
+          name='Wanting'
+          component={Wanting}
+          options={{
+            title: 'Better Wanting',
+            headerStyle: {
+              backgroundColor: '#0000ff',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
