6449c3334d65df0866c46ba499d96636c3aea342
Clayton Wong <claytonjwong@gmail.com>
2020-07-10 18:39:13 -0700

    add navigation between routes

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import Annoying from './components/Annoying';
+import Biases from './components/Biases';
+import Habits from './components/Habits';
 import Home from './components/Home';
+import Rewirements from './components/Rewirements';
+import Wanting from './components/Wanting';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Annoying' component={Annoying} />
+        <Stack.Screen name='Biases' component={Biases} />
+        <Stack.Screen name='Habits' component={Habits} />
         <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Rewirements' component={Rewirements} />
+        <Stack.Screen name='Wanting' component={Wanting} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/Home.js
+++ components/Home.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
-    );
-  }
-}
+const Home = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title='Annoying Features of the Mind'
+        onPress={() => navigation.navigate('Annoying')}
+      />
+      <Button
+        title='Overcome Biases'
+        onPress={() => navigation.navigate('Biases')}
+      />
+      <Button
+        title='Better Habits'
+        onPress={() => navigation.navigate('Habits')}
+      />
+      <Button
+        title='Rewirements'
+        onPress={() => navigation.navigate('Rewirements')}
+      />
+      <Button
+        title='Wanting'
+        onPress={() => navigation.navigate('Wanting')}
+      />
+    </View>
+  );
+};
 
 export default Home;
